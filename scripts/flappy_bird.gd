d49b2a38697961b7dae4f6fe94c68fb30f4be379
npnorman <npn.not@gmail.com>
2026-05-05 13:55:03 -0400

    add flapping to bird

--- scripts/flappy_bird.gd
+++ scripts/flappy_bird.gd
@@ -1,1 +1,26 @@
 extends Node2D
+
+@export var gravity : float = 500
+@export var flap_height : float = 200
+
+@onready var sprite: Sprite2D = $Sprite2D
+
+var velocity : float = 0
+
+func _process(delta: float) -> void:
+	
+	# on input, have the bird flap
+	if Input.is_action_just_pressed("flap"):
+		velocity = -flap_height
+		
+		# tilt up on flap
+		rotation_degrees = -45
+	
+	# create artificial gravity
+	# y is flipped compared to normal graphs
+	position.y += velocity * delta
+	velocity += gravity * delta
+	
+	# tilt downward
+	if velocity > 0:
+		rotation_degrees = lerpf(rotation_degrees, 90, 0.02)
